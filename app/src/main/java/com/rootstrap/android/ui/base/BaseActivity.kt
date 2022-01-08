6052730cc2e0e9c0758cf60bb6a47db2b00f70a7
Amaury <ricardo901130@gmail.com>
2022-01-08 20:32:42 -0300

    Initial commit

--- app/src/main/java/com/rootstrap/android/ui/base/BaseActivity.kt
+++ app/src/main/java/com/rootstrap/android/ui/base/BaseActivity.kt
@@ -6,6 +6,25 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @SuppressLint("Registered")
 @AndroidEntryPoint
-open class BaseActivity : AppCompatActivity() {
-    // TODO:.. add common functions
+open class BaseActivity : AppCompatActivity(), BaseView {
+    }
+
+    override fun showProgress() {
+        LoadingDialogUtil.showProgress(this)
+    }
+
+    override fun hideProgress() {
+        LoadingDialogUtil.hideProgress()
+    }
+
+    override fun showError(message: String?) {
+        LoadingDialogUtil.showError(message, this)
+    }
+
+    protected fun startActivityClearTask(activity: Activity) {
+        val intent = Intent(this, activity.javaClass)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
+    }
 }
